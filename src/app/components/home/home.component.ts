2c79ef835c59b8f022d45916e76a136e09e8cfec
WilderWR7 <wilder.mayta91@gmail.com>
2026-07-29 15:13:51 -0400

    feat: configure application routing and setup redirects

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
-
+  authService = inject(AuthService);
 }
