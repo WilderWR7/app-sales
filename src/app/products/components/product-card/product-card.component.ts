22298fd6a200dbc6607af64d63879092b862a44e
WilderWR7 <wilder.mayta91@gmail.com>
2026-07-29 20:33:41 -0400

    feat build cart drawer container and connect product card add to cart action

--- src/app/products/components/product-card/product-card.component.ts
+++ src/app/products/components/product-card/product-card.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Product } from '../../../core/models/product.model';
 import { StockBadgeComponent } from '../../../components/stock-badge/stock-badge.component';
+import { CartStore } from '../../../core/state/cart.store';
 
 @Component({
   selector: 'product-card',
@@ -10,5 +11,12 @@ import { StockBadgeComponent } from '../../../components/stock-badge/stock-badge
   templateUrl: './product-card.component.html',
 })
 export class ProductCardComponent {
+  private cartStore = inject(CartStore);
   @Input({ required: true }) product!: Product;
+
+  addToCart(): void {
+    if (this.product.stock > 0) {
+      this.cartStore.addItem(this.product);
+    }
+  }
 }
